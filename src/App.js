8e413a45961b0c4ad831edb73ac880f40783d3d8
Amy Lee <amy.sujunglee@gmail.com>
2023-09-19 22:20:30 -0700

    adding the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Daycare from "./components/Daycare";
+import Region from "./components/Region";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <h1>Find Daycare</h1>
+      <Region name="Burnaby" />
+      <Region name="Coquitlam" />
+      <Daycare />
     </div>
   );
 }
