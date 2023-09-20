9becad7ab3cf20c7ae4a1ac85540a0bd2485f871
Amy Lee <amy.sujunglee@gmail.com>
2023-09-19 22:24:13 -0700

    adding the react components

---
+++ src/components/Daycare.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Daycare = () => {
+    return (
+        <div>
+            <h3>Daycare name</h3>
+            <p>Daycare type</p>
+            <p>Max capacity</p>
+            <p>Address</p>
+            <p>Phone number</p>
+            <p>Owner name</p>
+            <p>Google ratings</p>
+            <p>Fraser health care status</p>
+        </div>
+    )
+}
+
+export default Daycare
---
+++ src/components/Region.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Region = (props) => {
+    return (
+        <div>
+            <button>{props.name}</button>
+        </div>
+    )
+}
+
+export default Region
