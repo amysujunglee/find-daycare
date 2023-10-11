84186acdb36899415d1e06defc732b90eb674f7d
Amy Lee <amy.sujunglee@gmail.com>
2023-10-10 20:20:07 -0700

    adding handleClick function

--- src/components/layout/RegionBtn.js
+++ src/components/layout/RegionBtn.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RegionBtn = (props) => {
     return (
-        <button className='bg-gray-200 p-2 border border-red-800 hover:bg-gray-400'>{props.name}</button>
+        <button className='bg-gray-200 p-2 border border-red-800 hover:bg-gray-400' onClick={props.onClick}>{props.name}</button>
     )
 }
 
--- src/pages/AllDaycares.js
+++ src/pages/AllDaycares.js
@@ -3,11 +3,14 @@ import DAYCARE_DATA from '../components/data'
 import RegionBtn from '../components/layout/RegionBtn'
 
 const AllDaycaresPage = () => {
+    const handleClick = () => {
+        console.log('clicked!')
+    }
     return (
         <div>
             <h1 className="text-3xl w-full text-center my-4">Find a daycare</h1>
             <div className="flex w-full mx-0 gap-3 justify-center">
-                <RegionBtn name="Burnaby" />
+                <RegionBtn name="Burnaby" onClick={handleClick} />
                 <RegionBtn name="Coquitlam" />
                 <RegionBtn name="New Westminster" />
                 <RegionBtn name="Port Moody" />
