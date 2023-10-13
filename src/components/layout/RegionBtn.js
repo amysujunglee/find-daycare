82463e9b2e59bdf95aa131972fd069e375b18868
Amy Lee <amy.sujunglee@gmail.com>
2023-10-13 11:23:13 -0700

    restructuing the buttons

--- src/components/layout/RegionBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RegionBtn = (props) => {
-    return (
-        <button className='bg-gray-200 p-2 border border-red-800 hover:bg-gray-400' onClick={props.onClick}>{props.name}</button>
-    )
-}
-
-export default RegionBtn
--- src/pages/AllDaycares.js
+++ src/pages/AllDaycares.js
@@ -1,22 +1,30 @@
 import DaycareList from '../components/daycares/DaycareList'
 import DAYCARE_DATA from '../components/data'
-import RegionBtn from '../components/layout/RegionBtn'
+import RegionBtns from '../components/layout/RegionBtns'
+import { useState } from 'react'
 
 const AllDaycaresPage = () => {
-    const handleClick = () => {
-        console.log('clicked!')
+    const [daycareData, setDaycareData] = useState(DAYCARE_DATA)
+    const regionItems = [...new Set(daycareData.map(daycare => daycare.region))]
+
+    console.log(regionItems)
+
+    const handleClick = (e) => {
+        const regionName = e.target.innerHTML;
+        const regionList = DAYCARE_DATA.map(daycare => {
+            return daycare.region;
+        })
+        const result = regionList.filter(item => item === regionName)
+        // console.log(result);
     }
+
     return (
         <div>
             <h1 className="text-3xl w-full text-center my-4">Find a daycare</h1>
             <div className="flex w-full mx-0 gap-3 justify-center">
-                <RegionBtn name="Burnaby" onClick={handleClick} />
-                <RegionBtn name="Coquitlam" />
-                <RegionBtn name="New Westminster" />
-                <RegionBtn name="Port Moody" />
-                <RegionBtn name="Maple Ridge" />
+                <RegionBtns regions={regionItems} onClick={handleClick} />
             </div>
-            <DaycareList daycares={DAYCARE_DATA} />
+            <DaycareList daycares={daycareData} />
         </div>
     )
 }
