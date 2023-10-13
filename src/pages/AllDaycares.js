fa42650222ec541d5516ad5605fa960cca1ae778
Amy Lee <amy.sujunglee@gmail.com>
2023-10-13 11:47:41 -0700

    fixing the button component

--- src/pages/AllDaycares.js
+++ src/pages/AllDaycares.js
@@ -1,30 +1,24 @@
 import DaycareList from '../components/daycares/DaycareList'
 import DAYCARE_DATA from '../components/data'
-import RegionBtns from '../components/layout/RegionBtns'
+import Buttons from '../components/layout/Buttons'
 import { useState } from 'react'
 
 const AllDaycaresPage = () => {
-    const [daycareData, setDaycareData] = useState(DAYCARE_DATA)
-    const regionItems = [...new Set(daycareData.map(daycare => daycare.region))]
+    const [daycares, setDaycares] = useState(DAYCARE_DATA)
+    const buttonItems = [...new Set(DAYCARE_DATA.map(item => item.region))]
 
-    console.log(regionItems)
-
-    const handleClick = (e) => {
-        const regionName = e.target.innerHTML;
-        const regionList = DAYCARE_DATA.map(daycare => {
-            return daycare.region;
-        })
-        const result = regionList.filter(item => item === regionName)
-        // console.log(result);
+    const filterDaycares = (region) => {
+        const daycareList = DAYCARE_DATA.filter((item) => item.region === region)
+        setDaycares(daycareList)
     }
 
     return (
         <div>
             <h1 className="text-3xl w-full text-center my-4">Find a daycare</h1>
             <div className="flex w-full mx-0 gap-3 justify-center">
-                <RegionBtns regions={regionItems} onClick={handleClick} />
+                <Buttons buttonItems={buttonItems} filterDaycares={filterDaycares} setDaycares={setDaycares} />
             </div>
-            <DaycareList daycares={daycareData} />
+            <DaycareList daycares={daycares} />
         </div>
     )
 }
