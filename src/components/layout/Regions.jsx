7f3b75794c6ce1d45a49dbf430af862387c17436
Amy Lee <amy.sujunglee@gmail.com>
2024-06-12 13:46:48 -0700

    fixing the region menus

--- src/components/layout/Regions.jsx
+++ src/components/layout/Regions.jsx
@@ -1,11 +1,11 @@
 import DAYCARE_DATA from "../utils/daycares";
 
-const daycaresNumber = (region) => {
-  const daycareList = DAYCARE_DATA.filter((item) => item.region === region);
-  return daycareList.length;
-};
-
 const Regions = ({ buttonItems, filterDaycares }) => {
+  const daycaresNumber = (region) => {
+    const daycareList = DAYCARE_DATA.filter((item) => item.region === region);
+    return daycareList.length;
+  };
+
   return (
     <div className="space-x-4">
       {buttonItems.map((item) => (
